06c8c1a67f6330a9c568b4d7d73f662e877bae3b
ashpak <ashpakv88@gmail.com>
2023-12-17 13:09:59 +0530

    added some comments

--- base64.c
+++ base64.c
@@ -12,11 +12,11 @@ void to_binary_string(char dest[], const char src[])
         {
             if (ch & (1 << j))
             {
-                strcat(binary_string, "1");
+                strcat(binary_string, "1"); // if the jth bit is set (i.e. 1) then append 1
             }
             else
             {
-                strcat(binary_string, "0");
+                strcat(binary_string, "0"); // else if the bit is not set (i.e. 0) then append 0
             }
         }
     }
@@ -34,14 +34,14 @@ void encode(char src[])
         fprintf(stderr, "Error: Could not allocate memory for char buffer.\n");
         exit(EXIT_FAILURE);
     }
-    to_binary_string(binary_string, src);
+    to_binary_string(binary_string, src); // convert the given string to binary string
     length = strlen(binary_string);
     while (length % 6 != 0)
     {
-        strcat(binary_string, "0");
+        strcat(binary_string, "0"); // pad the binary string with '0' bits if it's length is not divisible by 6
         length = strlen(binary_string);
     }
-    int num_of_segments = (length + 5) / 6;
+    int num_of_segments = (length + 5) / 6; // calculate the number segments, such that every segment contains 6 bits
     char segments[num_of_segments][7];
     for (i = 0; i < num_of_segments; i++)
     {
@@ -51,17 +51,17 @@ void encode(char src[])
     int decimal_values[num_of_segments];
     for (i = 0; i < num_of_segments; i++)
     {
-        decimal_values[i] = strtol(segments[i], NULL, 2);
+        decimal_values[i] = strtol(segments[i], NULL, 2); // now calculate the decimal value of each segment
     }
     char base64_string[1024];
     for (i = 0; i < num_of_segments; i++)
     {
-        base64_string[i] = b64_table[decimal_values[i]];
+        base64_string[i] = b64_table[decimal_values[i]]; // convert the decimal values to their respective values from base64 table
     }
     length = strlen(base64_string);
     while (length % 4 != 0)
     {
-        base64_string[i] = '=';
+        base64_string[i] = '='; // pad extra '=' until the length string is divisble by 4
         length = strlen(base64_string);
         i++;
     }
